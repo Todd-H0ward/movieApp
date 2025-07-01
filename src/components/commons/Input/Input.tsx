0b2ee759067549088d9ee52fad6df7b89825102b
Denis <dr144371@gmail.com>
2025-07-01 20:05:10 +0300

    Refactor Modal: update styles and add animation

--- src/components/commons/Input/Input.tsx
+++ src/components/commons/Input/Input.tsx
@@ -8,32 +8,18 @@ import s from './Input.module.scss';
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name?: string;
   label?: string;
-  type?: string;
-  placeholder?: string;
   error?: string | FieldError;
   className?: string;
 }
 
-const Input = forwardRef(
-  (
-    { name, label, type = 'text', placeholder, className, ...props }: InputProps,
-    ref: ForwardedRef<HTMLInputElement>,
-  ) => {
-    return (
-      <div className={s.root}>
-        {label && <label htmlFor={name}>{label}</label>}
-        <input
-          ref={ref}
-          className={clsx(s.input, className)}
-          name={name}
-          type={type}
-          placeholder={placeholder}
-          {...props}
-        />
-      </div>
-    );
-  },
-);
+const Input = forwardRef(({ name, label, className, ...props }: InputProps, ref: ForwardedRef<HTMLInputElement>) => {
+  return (
+    <div className={s.root}>
+      {label && <label htmlFor={name}>{label}</label>}
+      <input ref={ref} className={clsx(s.input, className)} name={name} {...props} />
+    </div>
+  );
+});
 
 Input.displayName = 'Input';
 
