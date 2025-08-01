e4a31427e239c2b43607934c53ca30b7ce54430e
Denis <dr144371@gmail.com>
2025-08-01 12:40:47 +0300

    Add MovieForm: extract form component

--- src/components/pages/HomePage/AddMovieModal/AddMovieModal.tsx
+++ src/components/pages/HomePage/AddMovieModal/AddMovieModal.tsx
@@ -1,58 +1,28 @@
 'use client';
 
-import { yupResolver } from '@hookform/resolvers/yup';
 import { CirclePlus } from 'lucide-react';
 import { useState } from 'react';
-import { Controller, FormProvider, useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 
 import type { AddMovieSchema } from '@/types/AddMovieSchema.ts';
 
 import Button from '@/components/commons/Button';
-import ImageInput from '@/components/commons/ImageInput';
-import Input from '@/components/commons/Input';
 import Modal from '@/components/commons/Modal';
-import TagsInput from '@/components/commons/TagsInput';
-import Textarea from '@/components/commons/Textarea';
+import MovieForm from '@/components/commons/MovieForm';
 
 import { addMovie } from '@/store/slices/movieSlice.ts';
-
-import { GENRES } from '@/constants/genres.ts';
-import { MOVIE_VALIDATION } from '@/constants/movieValidation.ts';
 
 import s from './AddMovieModal.module.scss';
 
 const AddMovieModal = () => {
-  const methods = useForm<AddMovieSchema>({
-    defaultValues: {
-      countries: [],
-      genres: [],
-    },
-    resolver: yupResolver(MOVIE_VALIDATION),
-  });
   const [isModalOpen, setIsModalOpen] = useState(false);
   const dispatch = useDispatch();
 
-  const {
-    register,
-    handleSubmit,
-    control,
-    reset,
-    formState: { errors },
-  } = methods;
-
   const onModalOpen = () => setIsModalOpen(true);
-  const onModalClose = () => {
-    setIsModalOpen(false);
-    reset();
-  };
+  const onModalClose = () => setIsModalOpen(false);
 
   const onSubmit = (data: AddMovieSchema) => {
-    if (methods.formState.errors.image) {
-      return;
-    }
-
     const newMovie = {
       id: uuidv4(),
       ...data,
@@ -69,102 +39,17 @@ const AddMovieModal = () => {
         <CirclePlus /> Добавить фильм
       </Button>
       <Modal isOpen={isModalOpen} onClose={onModalClose} title="Добавить фильм">
-        <FormProvider {...methods}>
-          <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
-            <Input
-              label="Название"
-              placeholder="Например: Властелин колец"
-              autoFocus
-              {...register('name')}
-              error={errors.name?.message}
-            />
-            <Input
-              label="Оригинальное название"
-              placeholder="Например: The Lord of the Rings"
-              {...register('originalName')}
-              error={errors.originalName?.message}
-            />
-            <Input
-              label="Год выхода"
-              type="number"
-              placeholder="Например: 2001"
-              step={1}
-              {...register('year')}
-              error={errors.year?.message}
-            />
-            <Controller
-              name="genres"
-              control={control}
-              render={({ field, fieldState }) => (
-                <TagsInput
-                  label="Жанр"
-                  placeholder="Выберите жанры"
-                  tags={field.value}
-                  hints={GENRES}
-                  changeTags={(genres) => {
-                    field.onChange(genres);
-                  }}
-                  error={fieldState.error?.message}
-                />
-              )}
-            />
-            <Controller
-              name="countries"
-              control={control}
-              render={({ field, fieldState }) => (
-                <TagsInput
-                  label="Страна"
-                  placeholder="Выберите страны"
-                  tags={field.value}
-                  changeTags={(countries) => {
-                    field.onChange(countries);
-                  }}
-                  error={fieldState.error?.message}
-                />
-              )}
-            />
-            <Input
-              label="Режиссёр"
-              placeholder="Например: Питер Джексон"
-              {...register('director')}
-              error={errors.director?.message}
-            />
-            <Input
-              label="Рейтинг"
-              type="number"
-              step={0.1}
-              placeholder="От 0.0 до 10.0"
-              {...register('rating')}
-              error={errors.rating?.message}
-            />
-            <Controller
-              name="image"
-              control={control}
-              render={({ field, fieldState }) => (
-                <ImageInput
-                  name="image"
-                  image={field.value}
-                  onChange={(image) => {
-                    field.onChange(image);
-                  }}
-                  error={fieldState.error?.message}
-                />
-              )}
-            />
-            <Textarea
-              label="Описание фильма"
-              placeholder="Например: Давным-давно в далёкой-далёкой галактике..."
-              {...register('description')}
-              error={errors.description?.message}
-            />
+        <MovieForm
+          onSubmit={onSubmit}
+          footer={
             <div className={s.controls}>
               <Button type="submit">Добавить</Button>
               <Button onClick={onModalClose} type="button" variant="filled">
                 Отменить
               </Button>
             </div>
-          </form>
-        </FormProvider>
+          }
+        />
       </Modal>
     </>
   );
--- src/components/pages/HomePage/EditMovieModal/EditMovieModal.tsx
+++ src/components/pages/HomePage/EditMovieModal/EditMovieModal.tsx
@@ -1,24 +1,16 @@
 'use client';
 
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useEffect, type MouseEvent } from 'react';
-import { Controller, FormProvider, useForm } from 'react-hook-form';
+import { type MouseEvent } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import type { AddMovieSchema } from '@/types/AddMovieSchema.ts';
 
 import Button from '@/components/commons/Button';
-import ImageInput from '@/components/commons/ImageInput';
-import Input from '@/components/commons/Input';
 import Modal from '@/components/commons/Modal';
-import TagsInput from '@/components/commons/TagsInput';
-import Textarea from '@/components/commons/Textarea';
+import MovieForm from '@/components/commons/MovieForm';
 
 import { selectEditMovie } from '@/store/selectors/movieSelectors.ts';
 import { deleteMovie, editMovie, setEditMovie } from '@/store/slices/movieSlice.ts';
-
-import { GENRES } from '@/constants/genres.ts';
-import { MOVIE_VALIDATION } from '@/constants/movieValidation.ts';
 
 import s from './EditMovieModal.module.scss';
 
@@ -26,35 +18,12 @@ const EditMovieModal = () => {
   const movie = useSelector(selectEditMovie);
   const dispatch = useDispatch();
 
-  const methods = useForm<AddMovieSchema>({
-    resolver: yupResolver(MOVIE_VALIDATION),
-  });
-
-  const {
-    register,
-    handleSubmit,
-    control,
-    reset,
-    formState: { errors },
-  } = methods;
-
-  useEffect(() => {
-    reset({
-      ...movie,
-      genres: movie?.genres ?? [],
-      countries: movie?.countries ?? [],
-    });
-  }, [movie, reset]);
-
   const onModalClose = () => {
     dispatch(setEditMovie(null));
-    reset();
   };
 
   const onSubmit = (data: AddMovieSchema) => {
-    if (methods.formState.errors.image || !movie) {
-      return;
-    }
+    if (!movie) return;
 
     dispatch(
       editMovie({
@@ -76,102 +45,18 @@ const EditMovieModal = () => {
 
   return (
     <Modal isOpen={!!movie} onClose={onModalClose} title="Редактировать фильм">
-      <FormProvider {...methods}>
-        <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
-          <Input
-            label="Название"
-            placeholder="Например: Властелин колец"
-            autoFocus
-            {...register('name')}
-            error={errors.name?.message}
-          />
-          <Input
-            label="Оригинальное название"
-            placeholder="Например: The Lord of the Rings"
-            {...register('originalName')}
-            error={errors.originalName?.message}
-          />
-          <Input
-            label="Год выхода"
-            type="number"
-            placeholder="Например: 2001"
-            step={1}
-            {...register('year')}
-            error={errors.year?.message}
-          />
-          <Controller
-            name="genres"
-            control={control}
-            render={({ field, fieldState }) => (
-              <TagsInput
-                label="Жанр"
-                placeholder="Выберите жанры"
-                tags={field.value}
-                hints={GENRES}
-                changeTags={(genres) => {
-                  field.onChange(genres);
-                }}
-                error={fieldState.error?.message}
-              />
-            )}
-          />
-          <Controller
-            name="countries"
-            control={control}
-            render={({ field, fieldState }) => (
-              <TagsInput
-                label="Страна"
-                placeholder="Выберите страны"
-                tags={field.value}
-                changeTags={(countries) => {
-                  field.onChange(countries);
-                }}
-                error={fieldState.error?.message}
-              />
-            )}
-          />
-          <Input
-            label="Режиссёр"
-            placeholder="Например: Питер Джексон"
-            {...register('director')}
-            error={errors.director?.message}
-          />
-          <Input
-            label="Рейтинг"
-            type="number"
-            step={0.1}
-            placeholder="От 0.0 до 10.0"
-            {...register('rating')}
-            error={errors.rating?.message}
-          />
-          <Controller
-            name="image"
-            control={control}
-            render={({ field, fieldState }) => (
-              <ImageInput
-                name="image"
-                image={field.value}
-                onChange={(image) => {
-                  field.onChange(image);
-                }}
-                error={fieldState.error?.message}
-              />
-            )}
-          />
-          <Textarea
-            label="Описание фильма"
-            placeholder="Например: Давным-давно в далёкой-далёкой галактике..."
-            {...register('description')}
-            error={errors.description?.message}
-          />
+      <MovieForm
+        movie={movie}
+        onSubmit={onSubmit}
+        footer={
           <div className={s.controls}>
             <Button type="submit">Сохранить</Button>
             <Button onClick={handleDelete} type="button" variant="filled">
               Удалить
             </Button>
           </div>
-        </form>
-      </FormProvider>
+        }
+      />
     </Modal>
   );
 };
